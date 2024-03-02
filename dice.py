72dcd4ae1c6b51d9c43ab334260b1081ab61f7a5
Gitkojo <161947993+Gitkojo@users.noreply.github.com>
2024-03-02 14:49:20 +0100

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+
+st.set_page_config(page_title="Dice game", page_icon="🎲", layout="wide")
+st.image('dice_animation.gif')
+st.title("Dice Game")
+st.subheader("Roll for your chance to win big")
+
+def dice_game():
+    a, b = np.random.randint(1,7,2)
+    st.write(f"Your numbers are {a} and {b}")
+    if  a==6 and b==6:
+        st.balloons()
+        return('congratulatons, you win')
+    else:
+        return("Almost there! Keep trying")
+if st.button("play"):
+    st.write(dice_game())
+
+    
+    
+
